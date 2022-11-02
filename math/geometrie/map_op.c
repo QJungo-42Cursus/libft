01e14f2e3d2a117ad0a20970d4920751bb8de54d
Quentin Jungo <qjungo@student.42lausanne.ch>
2022-11-02 11:06:10 +0100

    map op simplified

--- math/geometrie/map_op.c
+++ math/geometrie/map_op.c
@@ -6,7 +6,7 @@
 /*   By: qjungo <qjungo@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 10:17:02 by qjungo            #+#    #+#             */
-/*   Updated: 2022/11/02 10:23:35 by qjungo           ###   ########.fr       */
+/*   Updated: 2022/11/02 11:04:24 by qjungo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,13 @@
 #include "../vec/vec.h"
 #include "../ft_math.h"
 
-//		TODO : juste le calcul (sans la matrice)
-/*
-t_vec3	rotation_x(t_vec3 point, float angle)
-{
-	t_matrix	rotation_matrix;
-	t_matrix	n_point;
-	t_vec3		res;
-	angle = deg_to_rad(angle);
-	rotation_matrix = init_matrix(3, 3);
-	rotation_matrix.self[0][0] = 1;
-	rotation_matrix.self[0][1] = 0;
-	rotation_matrix.self[0][2] = 0;
-	rotation_matrix.self[1][0] = 0;
-	rotation_matrix.self[1][1] = cos(angle);
-	rotation_matrix.self[1][2] = -sin(angle);
-	rotation_matrix.self[2][0] = 0;
-	rotation_matrix.self[2][1] = sin(angle);
-	rotation_matrix.self[2][2] = cos(angle);
-	n_point = matrix_product(rotation_matrix, vec3_to_matrix(point));
-	res = matrix_to_vec3(n_point);
-	free_matrix(n_point);
-	free_matrix(rotation_matrix);
-	return (res);
-}
-*/
-
 t_vec3	rotation_x(t_vec3 point, float angle)
 {
 	t_vec3		b;
 
 	b.x = point.x;
 	b.y = point.y * cos(angle) + point.z * sin(angle);
-	b.z = (-point.y) * sin(angle) + point.z * cos(angle);
+	b.z = point.y * -sin(angle) + point.z * cos(angle);
 	return (b);
 }
 
@@ -54,7 +28,7 @@ t_vec3	rotation_z(t_vec3 a, float angle)
 {
 	t_vec3	b;
 
-	b.x = a.x * cos(angle) - a.y * sin(angle);
+	b.x = a.x * -cos(angle) + a.y * sin(angle);
 	b.y = a.x * sin(angle) + a.y * cos(angle);
 	b.z = a.z;
 	return (b);
