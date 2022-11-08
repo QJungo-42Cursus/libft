218f96d37e63aa889d93a115c9a1aca3fe4a4835
Quentin Jungo <quentin.jungo@gmail.com>
2022-11-08 11:00:58 +0100

    long_atoi added

--- nb/long_atoi.c
+++ nb/long_atoi.c
@@ -1,22 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   nb.h                                               :+:      :+:    :+:   */
+/*   long_atoi.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qjungo <qjungo@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/01 14:43:51 by qjungo            #+#    #+#             */
-/*   Updated: 2022/11/08 11:00:08 by qjungo           ###   ########.fr       */
+/*   Created: 2022/11/08 10:57:21 by qjungo            #+#    #+#             */
+/*   Updated: 2022/11/08 10:58:23 by qjungo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef NB_H
-# define NB_H
+#include "../libft.h"
 
-char	*ft_itoa(int n);
-int		ft_atoi(const char *string);
-long	long_atoi(const char *string);
-char	*utoa(unsigned int n);
-char	*itoa_base(unsigned long long int n, char *base, unsigned int n_base);
+long	long_atoi(const char *string)
+{
+	long		result;
+	int			i;
+	int			minus;
 
-#endif /* NB_H */
+	result = 0;
+	i = 0;
+	minus = 1;
+	while (is_in_charset(string[i], " \t\n\r\v\f"))
+		i++;
+	if (string[i] == '-')
+	{
+		minus = -minus;
+		i++;
+	}
+	else if (string[i] == '+')
+		i++;
+	while (is_in_charset(string[i], "0123456789"))
+	{
+		result = result * 10 + string[i] - 48;
+		i++;
+	}
+	return (result * minus);
+}
